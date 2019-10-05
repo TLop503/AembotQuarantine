30f6de6ce946b6ad6923ae7cf591b2a679b4d212
Will Richards <jwilt9000@gmail.com>
2019-10-05 09:46:02 -0700

    converted vuforia usage into a helper method

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Vuforia/VuforiaWrapper.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode.Helper.Vuforia;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+/**
+ * Class used to allow easy access to Vuforia methods
+ * @author Will Richards
+ */
+public class VuforiaWrapper {
+
+    //API Key for using Vuforia
+    private final String VUFORIA_KEY = "AXYyTwz/////AAAAGSZopKRDQkUWstJxKlGUqq0dBRoXhOdaJSvoDa+Wq00FKnLAUYSqP4OENgv1Q9BVMXFj9LG6L69Wc3fbJuVL7ZetjnVLIzjd9Cn9hvh5rp6HiSJ1rFrlfx0sgtkHda7a/B7HivbiVjfXq+Dta1L3IgQ+GSEmvdkXioXG6kA5ZDpQ8yG2o4cyzzvWTBzBQbrHumt1ek8qcGYAiv+552WCDMdTvrMC+NQf5R+CQdzub9pHC1rHEu6fCpQT9oq+zM6Vk3TMwxQ8KWhII0AXh5815A0yCvSyMqQFX++empRQj9o/hT6rSfz6hsHCeSg/RxptC2TpsfSos14e6rswW/Z/dh45fI3YTNfxxrkfzPjr2NAY";
+
+    //Localizer object
+    private VuforiaLocalizer vuforia;
+
+    //Trackable object set
+    private VuforiaTrackables trackables;
+
+    //The object we want to track
+    VuforiaTrackable skystoneTarget;
+
+    /**
+     * Constructs the variables required for the class
+     * @param hardwareMap a reference to the hardwareMap
+     */
+    public VuforiaWrapper(HardwareMap hardwareMap){
+
+        //Gets the camera id
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        //Creates a parameter set for the vuforia object
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        //Assigns the API key to the localizer object
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+
+        //Sets the camera to the back
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+        //Creates a new vuforia object with the parameters
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        //Loads the Skystone trackable set
+        trackables = vuforia.loadTrackablesFromAsset("Skystone");
+
+        //Create a new trackable with the skystone images as the src, and names it "Sky Stone"
+        skystoneTarget = trackables.get(0);
+        skystoneTarget.setName("Sky Stone");
+    }
+
+    /**
+     * Get the detected object's transform if no object return null
+     * @return detected objects transform, if non null
+     */
+    public String getDetectedTransform(){
+
+        try {
+            //If the target is present then return the trasnform if not return null
+            if(((VuforiaTrackableDefaultListener)skystoneTarget.getListener()).isVisible()){
+                return ((VuforiaTrackableDefaultListener)skystoneTarget.getListener()).getPosePhone().formatAsTransform();
+            }
+        }
+        catch (NullPointerException e){ return  null; }
+
+        return null;
+    }
+
+    /**
+     * Start Tracking
+     */
+    public void initVuforia(){
+        trackables.activate();
+    }
+
+    /**
+     * Stop Tracking
+     */
+    public  void stopVuforia(){
+        trackables.deactivate();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/VuforiaTrackTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/VuforiaTrackTest.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.teamcode.Helper.Vuforia.VuforiaWrapper;
 
 /**
  * This OpMode is used for testing skystone positions
@@ -19,55 +20,35 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 @Disabled
 public class VuforiaTrackTest extends OpMode {
 
-    OpenGLMatrix lastLocation = null;
+    //Create a reference to the vuforia wrapper
+    private VuforiaWrapper vuforia;
 
-    //Create the actual tracking object
-    VuforiaLocalizer vuforia;
 
-    //Holds the possible objects to track
-    VuforiaTrackables trackables;
-
-    //The object we want to track
-    VuforiaTrackable skystoneTarget;
-
-    //API Key for using Vuforia
-    private final String VUFORIA_KEY = "AXYyTwz/////AAAAGSZopKRDQkUWstJxKlGUqq0dBRoXhOdaJSvoDa+Wq00FKnLAUYSqP4OENgv1Q9BVMXFj9LG6L69Wc3fbJuVL7ZetjnVLIzjd9Cn9hvh5rp6HiSJ1rFrlfx0sgtkHda7a/B7HivbiVjfXq+Dta1L3IgQ+GSEmvdkXioXG6kA5ZDpQ8yG2o4cyzzvWTBzBQbrHumt1ek8qcGYAiv+552WCDMdTvrMC+NQf5R+CQdzub9pHC1rHEu6fCpQT9oq+zM6Vk3TMwxQ8KWhII0AXh5815A0yCvSyMqQFX++empRQj9o/hT6rSfz6hsHCeSg/RxptC2TpsfSos14e6rswW/Z/dh45fI3YTNfxxrkfzPjr2NAY";
-
+    /**
+     * Called when the init button is pressed
+     *
+     * Create a new vuforia wrapper for the vuforia variable
+     */
     @Override
     public void init() {
-
-        //Gets the camera
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-
-        //Creates a new set of localizer parameters based off the camera
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-
-        //Assign the parameters to the vuforia instance
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-        //Load the game images
-        trackables = this.vuforia.loadTrackablesFromAsset("Skystone");
-
-        //Create a new target with the skystone as the src
-        skystoneTarget = trackables.get(0);
-        skystoneTarget.setName("Sky Stone");
-
-
+        vuforia = new VuforiaWrapper(hardwareMap);
     }
 
+    /**
+     * At the start of the loop set the transform equal to the value
+     */
     @Override
     public void loop() {
+        String transform = vuforia.getDetectedTransform();
 
-        //If the skystone is visible the display the name and the X offset
-        if(((VuforiaTrackableDefaultListener)skystoneTarget.getListener()).isVisible()){
-            telemetry.addData("Visible Target: ", skystoneTarget.getName());
-
-            //The getPosePhone returns a location of the object relative to the phone's center, the 0th index is the X axis
-            telemetry.addData("X:", ((VuforiaTrackableDefaultListener) skystoneTarget.getListener()).getPosePhone().getData()[0]);
+        if(transform != null){
+            telemetry.addData("Transform: ", transform);
         }
+        else{
+            telemetry.addData("Transform: ", "No Object Detected");
+        }
+
+        telemetry.update();
     }
 
     /**
@@ -75,6 +56,6 @@ public class VuforiaTrackTest extends OpMode {
      * Activates the tracking
      */
     @Override
-    public  void init_loop(){ trackables.activate();}
+    public  void init_loop(){ vuforia.initVuforia(); }
 
 }
