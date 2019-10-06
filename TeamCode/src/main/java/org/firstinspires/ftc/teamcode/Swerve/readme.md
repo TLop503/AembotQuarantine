208cefead8d84320f5023a80aed2a9a06d039d8c
Will Richards <jwilt9000@gmail.com>
2019-10-06 11:03:21 -0700

    added readme to swerve module

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/readme.md
@@ -0,0 +1,3 @@
+## Swerve Module
+
+This module contains all the subcode required to drive differential swerve
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/VuforiaTrackTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/VuforiaTrackTest.java
@@ -4,12 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.teamcode.Helper.Vuforia.VuforiaWrapper;
 
 /**
