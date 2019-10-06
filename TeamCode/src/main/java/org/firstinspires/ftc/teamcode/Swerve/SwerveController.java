9327623faa6dfd0659ea3fd0228c2b340061f061
Will Richards <jwilt9000@gmail.com>
2019-10-06 12:44:08 -0700

    created the structure for controlling swerve

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/SwerveController.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.Swerve;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class created to drive the 2 swerve modules
+ * @author Will Richards
+ */
+public class SwerveController {
+
+    //Local variations of variables from the OpMode
+    private Gamepad gamepad1;
+    private HardwareMap hardwareMap;
+
+    //2 Swerve modules
+    private SwerveModule leftModule;
+    private SwerveModule rightModule;
+
+    private List<SwerveModule> moduleList = new ArrayList<>();
+
+    /**
+     * Constructs the swerve modules and passes information only acssesible in the OpMode to this class to be used
+     * @param gamepad1 a reference to the gamepad1
+     * @param hardwareMap a reference to the robot's hardware map
+     */
+    public SwerveController(Gamepad gamepad1, HardwareMap hardwareMap){
+        this.gamepad1 = gamepad1;
+        this.hardwareMap = hardwareMap;
+
+        //Creates 2 swerve modules
+        leftModule = new SwerveModule(ModulePosition.LEFT);
+        rightModule = new SwerveModule(ModulePosition.RIGHT);
+
+        //Adds the modules to a list for easy iteration
+        moduleList.add(leftModule);
+        moduleList.add(rightModule);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/SwerveModule.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.Swerve;
+
+/**
+ * Class used to control an individual swerve module
+ */
+public class SwerveModule {
+
+    //Holds the assigned module position be it left or right
+    ModulePosition modPos;
+
+    public SwerveModule(ModulePosition modPos){
+        this.modPos = modPos;
+    }
+}
