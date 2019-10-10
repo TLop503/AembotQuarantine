5970855c027b139ae7b81ce6af0fb2be2783343d
Troy Lopez <lopet576@gmail.com>
2019-10-10 14:25:19 -0700

    rack and Pinion test code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop.java
@@ -11,16 +11,28 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp(name = "Teleop",  group = "Teleop")
-@Disabled
 public class Teleop extends OpMode {
+
+    private Servo svTop;
+    private Servo svBottom;
+
+    private double svTopZero;
+    private double svBottomZero;
 
     /**
      * This method is run on robot init
      * Use as constructor
      */
+
+
+
     @Override
     public void init() {
+        svTop = hardwareMap.get(Servo.class, "svTop");
+        svBottom = hardwareMap.get(Servo.class, "svBottom");
 
+        svTopZero = svTop.getPosition();
+        svBottomZero = svBottom.getPosition();
     }
 
     /**
@@ -29,6 +41,14 @@ public class Teleop extends OpMode {
      */
     @Override
     public void loop() {
+        if (gamepad1.dpad_left){
+            svTop.setPosition(svTopZero - 0.5);
+            svBottom.setPosition(svBottomZero - 0.5);
+        }
+        else if (gamepad1.dpad_right){
+            svTop.setPosition(svTopZero + 0.5);
+            svBottom.setPosition(svTopZero + 0.5);
+        }
+        }
 
     }
-}
