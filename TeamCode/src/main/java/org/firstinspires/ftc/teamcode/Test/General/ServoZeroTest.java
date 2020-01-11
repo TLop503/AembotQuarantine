a46a53b428fc40dcadb52082eee3ceff98fbdbe4
Zane Othman <zane.othman@gmail.com>
2020-01-11 10:48:27 -0800

    Added a test to set a servo's position to 0.5.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/General/ServoZeroTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Test.General;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ServoZeroTest extends OpMode {
+    private Servo svBottom;
+    private boolean hasRun = false;
+
+    @Override
+    public void init() {
+        svBottom = hardwareMap.servo.get("svBottom");
+    }
+
+    @Override
+    public void loop() {
+        if (!hasRun) {
+            svBottom.setPosition(0.5);
+            hasRun = true;
+        } else {
+            requestOpModeStop();
+        }
+    }
+}
