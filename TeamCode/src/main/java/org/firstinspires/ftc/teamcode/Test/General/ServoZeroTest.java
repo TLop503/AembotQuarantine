3228dfd322d7a19c55d300d45ced2b5f7d2afa13
Zane Othman <zane.othman@gmail.com>
2020-01-11 11:34:05 -0800

    Found exact values for gripping stones with stacks on stacks.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/General/ServoZeroTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/General/ServoZeroTest.java
@@ -1,10 +1,10 @@
 package org.firstinspires.ftc.teamcode.Test.General;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name = "Zero svBottom", group = "Test")
+@TeleOp(name = "svBottom Test", group = "Test")
 public class ServoZeroTest extends OpMode {
     private Servo svBottom;
     private boolean hasRun = false;
@@ -16,11 +16,31 @@ public class ServoZeroTest extends OpMode {
 
     @Override
     public void loop() {
+        if (gamepad1.right_bumper) {
+            svBottom.setPosition(0.6);
+        } else if (gamepad1.left_bumper) {
+            svBottom.setPosition(0.3);
+        } else if (gamepad1.back) {
+            svBottom.setPosition(0.5);
+        } else if (gamepad1.right_trigger > 0) {
+            svBottom.setPosition(0.64);
+        } else if (gamepad1.left_trigger > 0) {
+            svBottom.setPosition(0.47);
+        }
+
+        /* Autonomous zeroing code
         if (!hasRun) {
             svBottom.setPosition(0.5);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
             hasRun = true;
         } else {
             requestOpModeStop();
         }
+         */
     }
 }
