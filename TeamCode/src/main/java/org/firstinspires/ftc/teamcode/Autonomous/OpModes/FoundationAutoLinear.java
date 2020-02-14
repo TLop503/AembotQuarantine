011fd0cea7e453bf298c3b730ff711d6efe8f9f3
Troy Lopez <lopet576@hsd.k12.or.us>
2020-02-14 10:42:11 -0800

    Foundation auto syntax brought up to date, distances fixed-ish

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/OpModes/FoundationAutoLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/OpModes/FoundationAutoLinear.java
@@ -11,18 +11,18 @@ import org.firstinspires.ftc.teamcode.Subsystems.StoneGripController;
 import org.firstinspires.ftc.teamcode.Swerve.SwerveController;
 import org.firstinspires.ftc.teamcode.Utilities.Hardware.Enums.IMUOrientation;
 
-@Autonomous(name = "Linear Foundation Auto V25 ", group = "Autonomous")
+@Autonomous(name = "Linear Foundation Auto V26", group = "Autonomous")
 public class FoundationAutoLinear extends LinearOpMode {
     private SwerveController swerve;
-    private StoneGripController stoneArms;
     private DcMotorSimple elevator;
 
     //Currently ignores side differences & start rules, array errors need to be patched first
 
+    //TODO: Test code since it was written for old swerve
+
     @Override
     public void runOpMode() throws InterruptedException {
         swerve = new SwerveController(null, hardwareMap, telemetry, IMUOrientation.VERTICAL, false);
-        stoneArms = new StoneGripController(hardwareMap, null, telemetry);
         elevator = hardwareMap.get(DcMotorSimple.class, "ElevatorMotor");
 
         waitForStart();
@@ -33,28 +33,27 @@ public class FoundationAutoLinear extends LinearOpMode {
         while (!strafeToPosition) {
             strafeToPosition = swerve.autoControlModules(75, 38, 0.4);
         }
-        /*
-        elevator.setPower(-0.5);
+
         sleep(1000);
-        elevator.setPower(0.5);
-        sleep(9500);
-
-         */
-
+        elevator.setPower(-0.5);
         sleep(1000);
 
         //Pull foundation back
         boolean drivenToFoundation = false;
         while (!drivenToFoundation) {
-            drivenToFoundation = swerve.autoControlModules(0, -4, 0.2);
+            drivenToFoundation = swerve.autoControlModules(0, -24, 0.2);
         }
+
+        elevator.setPower(0.5);
+        sleep(1000);
+        elevator.setPower(0.0);
 
         sleep(1000);
 
         //Sliiide to the left
         boolean strafeToPark = false;
         while (!strafeToPark) {
-            strafeToPark = swerve.autoControlModules(-260, 2.5, 0.2);
+            strafeToPark = swerve.autoControlModules(-45, 42, 0.2);
         }
 
     }
